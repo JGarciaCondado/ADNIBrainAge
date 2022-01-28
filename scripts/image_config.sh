48317e723653ae0f5a60c785c5fc4bbe6cbf8808
JGarciaCondado <jorgegarciacondado@gmail.com>
2022-01-28 10:14:03 +0100

    Add absolute project paths

--- scripts/image_config.sh
+++ scripts/image_config.sh
@@ -3,10 +3,13 @@
 #Usage TODO
 
 #Image folders
-image_folder=$1
+project_path=$1
+image_folder=$2
 
 # Command useful to unstack all of the subject images
 # mv **/**/**/**/*.nii ./
+
+cd ${project_path}
 
 #Obtain patient lits
 ls ${image_folder} | awk '{ print $1 }' > DATA/patient_list.txt
@@ -15,14 +18,19 @@ mkdir -p SUBJECTS
 
 count=1
 
-for i in $(cat DATA/patient_list.txt)
+for i in $(cat ${project_path}/DATA/patient_list.txt)
 do
-    subject_code=sub$(printf "%03d" $count)
-	
-    #In this .tsv file you can trace the original subject code and the new BIDS one
-	echo "${subject_code}	${i:5:10}" >> DATA/participants.tsv
 
-    cp ${image_folder}/${i} SUBJECTS/${subject_code}_T1w.nii.gz
+    cd ${project_path}/SUBJECTS
+
+    subject_code=sub$(printf "%03d" $count)
+    mkdir -p ${subject_code}
+	cd ${subject_code}
+
+    #In this .tsv file you can trace the original subject code and the new BIDS one
+	echo "${subject_code}	${i:5:10}" >> ${project_path}/DATA/participants.tsv
+
+    cp ${image_folder}/${i} ${project_path}/SUBJECTS/${subject_code}/${subject_code}_T1w.nii
 
 	count=$[$count +1]
 done
